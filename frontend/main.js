fb5c032506fcbbb43d530c4359bef4eaa319b95c
Nathan Mausehund <ndmausehund@Mousedog.local>
2022-09-12 14:19:49 -0500

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded',(event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterfinal.azurewebsites.net/api/GetResumeCounter?code=Fb2DLDjIfkcclhYimcq7QaS0yJkIse7YVg4pAxgzqK4KAzFuXCGQuw==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Wensite called function API");
